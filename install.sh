4a91a446e37f49dc7a0d602352596f704444959f
Dev Soni <imdevsoni5505@gmail.com>
2026-04-25 18:17:47 +0000

    Installation script added graphically

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+if [[ ${EUID} != 0 ]]; then
+  echo "$0: sudo privileges are required to run this script!"
+  exit 1
+fi
+
+filename="${BASH_SOURCE[0]}"
+DIR=$(dirname "${filename}")
+if [[ -f "${DIR}/loading.sh" ]]; then
+  chmod u+x "${DIR}/loading.sh"
+  sudo cp "${DIR}/loading.sh" /usr/local/bin/
+  echo "Installation completed"
+  echo "Run tool using: loading"
+else
+  echo "$0: loading.sh script is missing!"
+  exit 1
+fi
